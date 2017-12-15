d3434bf762e454a3bb9948682c7ac63d33a653b0
Justyna Mackoś <j.mackos@wp.pl>
2017-12-15 10:42:20 +0100

    Initial commit

---
+++ Source/AssetsTest/AssetsTestGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "AssetsTestGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ASSETSTEST_API AAssetsTestGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
